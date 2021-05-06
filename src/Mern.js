e402ebb087da32a27b7573ee029bf2f191d6e1ea
Gurpreet5028 <gurpreet@dogoodthings.co.nz>
2021-05-06 21:14:46 +1200

    file cleared

--- src/Mern.js
+++ src/Mern.js
@@ -4,10 +4,7 @@ import { BrowserRouter as Router, Route} from "react-router-dom";
 
 import Navbar from "./components/navbar.component"
 import Footer from "./components/Footer"
-import ExercisesList from "./components/exercises-list.component";
-import EditExercise from "./components/edit-exercise.component";
-import CreateExercise from "./components/create-exercise.component";
-import CreateUser from "./components/create-user.component";
+
 
 
 import './mern.css';
